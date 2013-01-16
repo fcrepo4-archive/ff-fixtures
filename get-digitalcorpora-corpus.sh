66b8c1d3f43ae4d7561270d5eb82fbc3c75ba468
Chris Beer <cabeer@stanford.edu>
2013-01-16 07:15:22 -0800

    let the user pass in the bagit path

--- get-digitalcorpora-corpus.sh
+++ get-digitalcorpora-corpus.sh
@@ -2,11 +2,17 @@
 
 CORPUS_NAME=thread${THREAD_ID-0}
 PROCESSING_DIR=govdocs
+BAGIT_PY_CMD=${BAGIT_PY-./bagit.py}
 
 command -v curl >/dev/null || { echo "curl command not found, aborting"; exit 1; }
-command -v bagit.py >/dev/null || { echo "bagit.py command not found, aborting"; exit 1; }
+command -v python >/dev/null || { echo "python command not found, aborting"; exit 1; }
 command -v unzip >/dev/null || { echo "unzip command not found, aborting"; exit 1; }
 
+if [ ! -f $BAGIT_PY_CMD ]; then
+	echo "bagit.py command not found, aborting."
+	echo "Hint: try calling this script with an explicit BAGIT_PY, e.g.:"
+	echo "$ BAGIT_PY=./path/to/bagit.py $0"; exit 1;
+fi
 
 touch .gitignore
 mkdir ${PROCESSING_DIR}
@@ -27,7 +33,7 @@ echo " == Extracting Corpus"
 unzip ${PROCESSING_DIR}/${CORPUS_NAME}.zip -d ${PROCESSING_DIR}
 
 echo " == Bagging Corpus objects"
-bagit.py --contact-name 'Digital Corpora' ${PROCESSING_DIR}/[0-9]*
+python $BAGIT_PY_CMD --contact-name 'Digital Corpora' ${PROCESSING_DIR}/[0-9]*
 
 echo " == Moving bagged objects"
 for i in $( ls ${PROCESSING_DIR}/[0-9]* ); do
