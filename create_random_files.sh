1addd912787edd8c3eded41970daee8481c181ef
fasseg <frank.asseg@googlemail.com>
2013-01-18 16:41:46 +0100

    added generation of md5 manifest so it can be used by the jmeter tests

--- create_random_files.sh
+++ create_random_files.sh
@@ -5,6 +5,8 @@ FILE_PREFIX=random_
 FILE_SUFFIX=.data
 INPUT_SIZES=random_sizes.data
 DD_BIN=`which dd`
+MD5SUM_BIN=`which md5sum`
+RM_BIN=`which rm`
 COUNT=1
 
 # first create the output dir if it does not exist
@@ -16,6 +18,15 @@ for LINE in $(cat $INPUT_SIZES)
 do
 	PATH=${OUTPUT_DIR}/${FILE_PREFIX}${COUNT}${FILE_SUFFIX}
 	echo "creating file ${PATH} of size ${LINE} MB"
-	$DD_BIN if=/dev/urandom of=$PATH bs=1048576 count=${LINE}
+#	$DD_BIN if=/dev/urandom of=$PATH bs=1048576 count=${LINE}
+	$DD_BIN if=/dev/urandom of=$PATH bs=1024 count=${LINE}
 	let COUNT=COUNT+1
 done
+
+cd objects/random
+$RM_BIN  manifest-md5.txt
+for FILE in *
+do
+	$MD5SUM_BIN $FILE >> manifest-md5.txt
+done
+cd ../..
